e84e70762b19821f008fc24f2f0944fb50fd66bc
Mohammad <dadsetanpor@gmail.com>
2024-10-28 00:51:19 +0330

    feat(homepage): add search section

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,12 @@
 import Banner from "./Banner"
-
+import Search from "./Search"
 
 
 export default function Main() {
     return (
         <main>
             <Banner />
-
+            <Search />
 
 
         </main>
---
+++ src/components/Main/Search.tsx
@@ -0,0 +1,21 @@
+import SimpleContainer from "../Container"
+import BoxBasic from "../Box"
+import BasicGrid from "../Grid"
+import Grid from "@mui/system/Grid"
+export default function Search() {
+    return (
+        <SimpleContainer typ={"section"} custom={{ padding: "4em 2em", backgroundColor: 'rgb(237,237,237)' }}>
+            <BoxBasic custom={{ width: "100%" }}>
+               
+            </BoxBasic>
+
+
+        </SimpleContainer>
+
+
+
+
+
+
+    )
+}
