d25007e05ec38ec27c453d771d28afd790bf69e4
Mohammad <dadsetanpor@gmail.com>
2024-10-28 10:04:37 +0330

    feat(homepage): add Inputbox component to search section

--- src/components/Main/Search.tsx
+++ src/components/Main/Search.tsx
@@ -3,6 +3,8 @@ import BoxBasic from "../Box"
 import BasicGrid from "../Grid"
 import Grid from "@mui/system/Grid"
 import Typtext from "../Typography"
+import Inputbox from "../Inputbox"
+import SearchIcon from '@mui/icons-material/Search';
 export default function Search() {
     return (
         <SimpleContainer typ={"section"} custom={{ padding: "4em 2em", backgroundColor: 'rgb(237,237,237)' }}>
@@ -10,13 +12,13 @@ export default function Search() {
                 <BasicGrid gridGap={0}>
 
                     <Grid size={6}>
-                        <BoxBasic custom={{maxWidth:"70%"}}>
-                            <Typtext variant={"h4"} typ={"p"} custom={{ lineHeight:"1em"}}>Find a domain name for your website</Typtext>
+                        <BoxBasic custom={{ maxWidth: "70%" }}>
+                            <Typtext variant={"h4"} typ={"p"} custom={{ lineHeight: "1em" }}>Find a domain name for your website</Typtext>
                         </BoxBasic>
                     </Grid>
                     <Grid size={6}>
                         <BoxBasic>
-
+                            <Inputbox typ={"search"} variant={"outlined"} icon={<SearchIcon/>} placeholder={"search your product"} />
                         </BoxBasic>
                     </Grid>
 
