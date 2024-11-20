cbdc5221a0b446437c36aff9d3f91b5ef17c19ba
Mohammad <dadsetanpor@gmail.com>
2024-11-20 23:52:13 +0330

    feat(products page): add search's result text

--- src/components/Products/products.tsx
+++ src/components/Products/products.tsx
@@ -1,18 +1,20 @@
-
+import DirectionStack from "../Flexcontainer"
 import BoxBasic from "../Box"
 import BasicGrid from "../Grid"
 import { Grid2 } from "@mui/material"
 import Typtext from "../Typography"
 export default function Products() {
     return (
-        <BoxBasic typ={"section"} custom={{padding:"3em 2em"}}>
-            <BoxBasic>
+        <BoxBasic typ={"section"} custom={{ padding: "0 2em" }}>
+            <BoxBasic  custom={{padding:"3em 0"}}>
                 <BasicGrid gridGap={0}>
-                    <Grid2 size={6} sx={{alignItems:"center" , display:"flex"}}><BoxBasic><Typtext variant={"h2"} typ={'h1'} custom={{maxWidth:"90%", fontSize:"3.2em"}}>Make any template yours with ease.</Typtext></BoxBasic></Grid2>
-                    <Grid2 size={6} sx={{alignItems:"center" , display:"flex"}}><BoxBasic><Typtext variant={"body1"} custom={{maxWidth:"90%",}}>Whether you need a portfolio website, an online store, or a personal blog, you can use Squarespace's customizable and responsive website templates to get started.</Typtext></BoxBasic></Grid2>
+                    <Grid2 size={6} sx={{ alignItems: "center", display: "flex" }}><BoxBasic><Typtext variant={"h2"} typ={'h1'} custom={{ maxWidth: "90%", fontSize: "3.2em" }}>Make any template yours with ease.</Typtext></BoxBasic></Grid2>
+                    <Grid2 size={6} sx={{ alignItems: "center", display: "flex" }}><BoxBasic><Typtext variant={"body1"} custom={{ maxWidth: "90%", }}>Whether you need a portfolio website, an online store, or a personal blog, you can use Squarespace's customizable and responsive website templates to get started.</Typtext></BoxBasic></Grid2>
                 </BasicGrid>
             </BoxBasic>
+            <DirectionStack gapspace={0} direct={"row"} sx={{ justifyContent: "space-between" }}><Typtext>29 result</Typtext>
 
+            </DirectionStack>
 
 
 
